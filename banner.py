4c0c797f8836edb2450fb00bdbacef4a1572dd23
andrei-akopian <andrei.a.akopian@gmail.com>
2023-04-26 10:57:08 -0400

    added banner

--- banner.py
+++ banner.py
@@ -1,13 +1,19 @@
 #TODO add stats etc
 def displayBanner(Vocsets): #TODO rename banner to something else
+    #load in
     import random
-    with open("/Users/andrei/Documents/Coding/Projects/AnsiConverter/logoCompressed.txt") as f:
+    bannertext=[]
+    with open("logoPixels.txt","r") as f:
         logoRaw=f.readlines()
+    with open("bannertext.txt","r") as f:
+        bannertext=f.readlines()
 
     logo=[]
     for logoRaw in logoRaw:
         cords=logoRaw.strip("\n").split(",")
         logo.append((int(cords[0]),int(cords[1])))
+    size=logo[0][:]
+    del logo[0]
 
     YELLOW="\033[38;5;220m"
     BLACK="\033[38;5;240m"
@@ -27,9 +33,14 @@ def displayBanner(Vocsets): #TODO rename banner to something else
     wordi=0
     pixelI=0
     casefliper=0
+    color=BLACK
 
-    while y<32:
-        while x<32:
+    line=""
+    banner=[]
+    maxlen=0
+
+    while y<size[1]:
+        while x<size[0]:
             if logo[pixelI]==(x,y):
                 if len(word)==0:
                     if casefliper:
@@ -38,15 +49,21 @@ def displayBanner(Vocsets): #TODO rename banner to something else
                     else:
                         casefliper=1
                         word=wordlist[random.randint(0,len(wordlist)-1)].lower()
-                print(YELLOW+word[wordi],end=BLACK)
+                if color==BLACK:
+                    line+=YELLOW
+                    color=YELLOW
+                line+=word[wordi]
                 pixelI+=1
                 wordi+=1
             else:
+                if color==YELLOW:
+                    line+=BLACK
+                    color=BLACK
                 if len(word)==0:
-                    print(" ",end="")
+                    line+=" "
                     casefliper=0
                 else: 
-                    print(BLACK+word[wordi],end="")
+                    line+=word[wordi]
                     wordi+=1
             if wordi>=len(word):
                 word=""
@@ -54,11 +71,25 @@ def displayBanner(Vocsets): #TODO rename banner to something else
             x+=1
             if pixelI==len(logo):
                 break
-        print(word[wordi:])
+
+        if color==YELLOW:
+            line+=BLACK
+            color=BLACK
+        line+=word[wordi:]
+        #save the results
+        maxlen=max(maxlen,size[0]+len(word)-wordi)
+        banner.append([line,size[0]+len(word)-wordi])
+        #reset for next cycle
+        line=""
         word=""
         wordi=0
         x=0
         y+=1
         if pixelI==len(logo):
-            print("\033[0m")
             break
+
+    for lineI in range(len(banner)):
+        banner[lineI][0]+=" "*(maxlen-banner[lineI][1])+"\033[0m"
+        print(banner[lineI][0],bannertext[lineI],end="") #TODO deal with bannertext \n when adding the config stats etc.
+    print()
+    return banner
